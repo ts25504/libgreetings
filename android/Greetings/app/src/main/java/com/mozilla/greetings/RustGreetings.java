21ca5cd6c250d1c0ea3f04204edace461bd7705f
Sheng Tang <ts25504@gmail.com>
2019-12-09 15:02:06 +0800

    Update RustGreetings.java

--- android/Greetings/app/src/main/java/com/mozilla/greetings/RustGreetings.java
+++ android/Greetings/app/src/main/java/com/mozilla/greetings/RustGreetings.java
@@ -1,9 +1,5 @@
 package com.mozilla.greetings;
 
-/**
- * Created by TangSheng (tangsheng02@meituan.com) on 27/12/2017.
- */
-
 public class RustGreetings {
 
     private static native String greeting(final String pattern);
